967a8fb201e70260f10676ca9df215df2a9fc5ea
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2024-11-14 12:01:08 -0300

    settings.py

--- mi_experimento/settings.py
+++ mi_experimento/settings.py
@@ -19,14 +19,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
+import environ
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# Inicializa el entorno
+env = environ.Env(
+    DEBUG=(bool, False)  # Configura el valor por defecto de DEBUG como False
+)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+# Lee el archivo .env si existe
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# Usar las variables de entorno directamente
+SECRET_KEY = env('SECRET_KEY', default='unsafe-secret-key')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='localhost').split(',')
 
 
 # Application definition
