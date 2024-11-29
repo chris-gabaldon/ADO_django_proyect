64e2e8ea23456addccecd97efcc405888db49461
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2024-11-29 16:42:20 -0300

    Delete experimento directory

--- experimento/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Trial,Block, ExperimentSession
-# Registra el modelo para que sea visible en la administración
-admin.site.register(Trial)
-admin.site.register(Block)
-admin.site.register(ExperimentSession)
